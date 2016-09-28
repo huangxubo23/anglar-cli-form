8f2da7ede0109f6fa202c8bf3fbd772523f78f59
huangxubo23 <732150595@qq.com>
2016-09-28 20:23:52 +0800

    Add signup

---
+++ src/app/signup/new-user.ts
@@ -0,0 +1,22 @@
+export class NewUser {
+    userName: string = null;
+    email: string = null
+    telephone: string = null;
+    password: string = null;
+    sex: string = 'male';
+    age: number = null;
+    address: string = null;
+    habbies: string[] = [];
+    constructor() {
+        //this.address = new Address();
+        //this.habbies = [];
+    }
+}
+
+export class Address {
+    country: string = null;
+    province: string = null;
+    city: string = null;
+    postcode: string = null;
+    street: string = null;
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+
+import { FormValidationService } from '../common/form-validation.service';
+import { NewUser } from './new-user';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
+  viewProviders: [ReactiveFormsModule],
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  newUser: NewUser;
+  newUserForm: FormGroup;
+  constructor(private formBuilder:FormBuilder) {
+    this.newUser = new NewUser();
+    console.log(this.newUser);
+  }
 
   ngOnInit() {
+    console.log('signup init');
+    let formGroupObj = this.generateForm(this.newUser);
+    this.newUserForm = this.formBuilder.group(formGroupObj);
+  }
+
+  generateForm(data: NewUser) {
+    let formGroupObj = {};
+    for (let property in data) {
+      if (!data.hasOwnProperty(property))
+        return;
+
+      var formCtrlValidators = [];
+      switch (property) {
+        case 'userName':
+        case 'password':
+          formCtrlValidators.push(Validators.required);
+          formGroupObj[property] = [this.newUser[property], formCtrlValidators];
+          break;
+        case 'email':
+          formCtrlValidators.concat([Validators.required, FormValidationService.emailValidator]);
+          formGroupObj[property] = [this.newUser[property], formCtrlValidators];
+        case 'age':
+          formCtrlValidators.push(FormValidationService.integerValidator(0, 150));
+          formGroupObj[property] = [this.newUser[property], formCtrlValidators];
+          break;
+        default:
+          break;
+      }
+    }
+
+    return formGroupObj;
   }
 
 }
