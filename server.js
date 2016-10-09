162190bf1f0a818bf2850be62751491dc0cda857
huangxubo23 <732150595@qq.com>
2016-10-09 11:26:44 +0800

    Use nodemon and ES6

--- server.js
+++ server.js
@@ -4,38 +4,38 @@
  * License: MIT
  */
 
-var path = require('path');
+const path = require('path');
 
-var bcrypt = require('bcryptjs')
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var express = require('express');
-var expressValidator = require('express-validator');
-var flash = require('express-flash');
-var logger = require('morgan');
-var chalk = require('chalk');
-var mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const expressValidator = require('express-validator');
+const flash = require('express-flash');
+const logger = require('morgan');
+const chalk = require('chalk');
+const mongoose = require('mongoose');
 
-var config = require('./server/config');
+const config = require('./server/config');
 
 /**
  * Create Express server.
  */
-var app = express();
+const app = express();
 
-var userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, lowercase: true },
     password: { type: String, select: false },
     userName: String
 });
 
 userSchema.pre('save', function (next) {
-    var user = this;
+    let user = this;
     if (!user.isModified('password')) {
         return next();
     }
-    bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(user.password, salt, function (err, hash) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
             user.password = hash;
             next();
         });
@@ -43,27 +43,27 @@ userSchema.pre('save', function (next) {
 });
 
 userSchema.methods.comparePassword = function (password, done) {
-    bcrypt.compare(password, this.password, function (err, isMatch) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
         done(err, isMatch);
     });
 };
 
-var User = mongoose.model('Angular_User', userSchema);
+const User = mongoose.model('Angular_User', userSchema);
 
 /**
  * Connect to MongoDB.
  */
 mongoose.Promise = global.Promise;
 mongoose.connect(config.MONGO_URI);
-mongoose.connection.on('connected', function () {
+mongoose.connection.on('connected', () => {
     console.log('%s MongoDB connection established!', chalk.green('✓'));
 });
-mongoose.connection.on('error', function (err) {
+mongoose.connection.on('error', (err) => {
     console.log('%s Could not connect to MongoDB. Did you forget to run `mongod`?', chalk.red('✗'));
 });
 
 
-app.set('port', process.env.NODE_PORT || 3000);
+app.set('port', process.env.NODE_PORT || 5000);
 app.set('host', process.env.NODE_IP || 'localhost');
 app.use(cors());
 app.use(logger('dev'));
@@ -74,8 +74,8 @@ app.use(expressValidator());
 
 // Force HTTPS on Heroku
 if (app.get('env') === 'production') {
-    app.use(function (req, res, next) {
-        var protocol = req.get('x-forwarded-proto');
+    app.use((req, res, next) => {
+        const protocol = req.get('x-forwarded-proto');
         protocol === 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
     });
 }
@@ -86,9 +86,9 @@ app.use(express.static(path.join(__dirname, './src')));
  | Get user information
  |--------------------------------------------------------------------------
  */
-app.get('/api/user', function (req, res) {
+app.get('/api/user', (req, res) => {
     //57f9120fa3fd5308e0d3d0d0
-    User.findById(req.query.userId, function (err, user) {
+    User.findById(req.query.userId, (err, user) => {
         res.send(user);
     });
 });
@@ -98,14 +98,14 @@ app.get('/api/user', function (req, res) {
  | Create Email and Password Account
  |--------------------------------------------------------------------------
  */
-app.post('/api/sigup', function (req, res, next) {
+app.post('/api/sigup', (req, res, next) => {
     req.assert('email', 'Email is required').notEmpty();
     req.assert('email', 'Email is not valid').isEmail();
     req.assert('password', 'Password must be at least 4 characters long').len(4);
     req.assert('confirmPassword', 'Password do not match').equals(req.body.password);
     req.sanitize('email').normalizeEmail({ remove_dots: false });
 
-    var errors = req.validationErrors(true);
+    const errors = req.validationErrors(true);
 
     if (errors) {
         //req.flash('errors', errors);
@@ -117,18 +117,16 @@ app.post('/api/sigup', function (req, res, next) {
         return res.status(404).send({ message: errors });
     }
 
-    User.findOne({ email: req.body.email }, function (err, existingUser) {
-        console.log('find');
+    User.findOne({ email: req.body.email }, (err, existingUser) => {
         if (existingUser) {
             return res.status(409).send({ message: 'Email is already taken' });
         }
-        var user = new User({
+        const user = new User({
             email: req.body.email,
             password: req.body.password,
             userName: req.body.userName
         });
-        user.save(function (err, result) {
-            console.log('save:' + err);
+        user.save((err, result) => {
             if (err) {
                 return res.status(500).send({ message: err.message });
             }
@@ -143,6 +141,6 @@ app.post('/api/sigup', function (req, res, next) {
  | Start the Server
  |--------------------------------------------------------------------------
  */
-app.listen(app.get('port'), app.get('host'), function () {
+app.listen(app.get('port'), app.get('host'), () => {
     console.log('Express sercer listening on: ' + app.get('host') + ':' + app.get('port'));
 });
